eacc62f2e0e9d6de17c42a6c8c85238edcf417af
Rage <91225758+0-X0deX-o@users.noreply.github.com>
2022-01-18 11:43:01 -0500

    writing js file at painstakingly slow pace

--- TOP/library/library.js
+++ TOP/library/library.js
@@ -1,1 +1,19 @@
 //comment for commit
+
+myLibrary = [];
+
+const bookCreator = (title, author, pubYear, read, numPages) => {
+    let timeReqToRead = (numPages/80).toFixed(2);
+    return { title, author, pubYear, read, numPages, timeReqToRead }; 
+};
+
+let devilWearsPrada = bookCreator(`The Devil Wears Prada`, `Lauren Weisberger`, 2003, false, 384);
+
+
+
+console.log(myLibrary);
+console.log(devilWearsPrada);
+myLibrary.push(devilWearsPrada);
+console.log(myLibrary);
+
+`Beware of all enterprises that require new clothes -Henry David Thoreau`
